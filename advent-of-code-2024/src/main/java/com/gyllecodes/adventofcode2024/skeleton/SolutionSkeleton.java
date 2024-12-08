509ba20f82cbe3135355a73628946431f0af2f73
Erik Gylleus <144366622+ergyl@users.noreply.github.com>
2024-12-08 21:54:24 +0100

    remove comment

--- advent-of-code-2024/src/main/java/com/gyllecodes/adventofcode2024/skeleton/SolutionSkeleton.java
+++ advent-of-code-2024/src/main/java/com/gyllecodes/adventofcode2024/skeleton/SolutionSkeleton.java
@@ -5,15 +5,6 @@ import com.gyllecodes.adventofcode2024.Main;
 
 import java.util.List;
 
-/**
- * All solutions are implemented from this.
- * The createSrc(int day) method copies this skeleton class file
- * to a new file (e.g., Day01.java) in a corresponding directory (e.g., day01).
- * It dynamically updates placeholders in the skeleton file to reflect the day's
- * number (e.g., replacing SolutionSkeleton with Day01).
- *
- * @see Main
- */
 public class SolutionSkeleton extends DayTemplate {
 
     protected SolutionSkeleton(String filename) {
